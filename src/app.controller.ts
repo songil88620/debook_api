9e7d6989d499f95504baa07c8f406d1df7f9fecb
songil88620 <songil88620@gmail.com>
2024-12-10 14:33:28 +0800

    debug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/public.decorator';
 
 @Controller('link')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   @Redirect(
     'debook:///(protected)/main/book/0009ebde-b2e7-479c-8737-67acbc3f8ac3',
     302,
