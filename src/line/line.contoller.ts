77e89200b5eeed5d1be6ec153c8ab883a2485057
songil88620 <songil88620@gmail.com>
2024-12-20 13:36:23 +0800

    debug

--- src/line/line.contoller.ts
+++ src/line/line.contoller.ts
@@ -15,11 +15,12 @@ import {
 } from '@nestjs/common';
 import { ApiConsumes, ApiResponse } from '@nestjs/swagger';
 import { FirebaseAuthGuard } from 'src/auth/auth.guard';
-import { User } from 'src/user/user.decorator';
+import { Tester, User } from 'src/user/user.decorator';
 import { LineService } from './line.service';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { UploadService } from 'src/upload/upload.service';
 import { LineCreateDto } from './dtos';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller('lines')
 export class LineController {
@@ -32,12 +33,69 @@ export class LineController {
   @Post()
   @ApiConsumes('multipart/form-data')
   @UseGuards(FirebaseAuthGuard)
+  // @Public()
   @UseInterceptors(
     AnyFilesInterceptor({ limits: { fileSize: 100 * 1024 * 1024 } }),
   )
   async createLine(
     @UploadedFiles() files: Express.Multer.File[],
     @User() user: any,
+    @Body() data: LineCreateDto,
+  ) {
+    try {
+      const videoFile = files.find((file) => file.fieldname === 'file');
+      const thumbnailFile = files.find(
+        (file) => file.fieldname === 'thumbnail',
+      );
+
+      if (!videoFile.mimetype.match(/video\/(mp4)/)) {
+        throw new BadRequestException('Video file must be of type MP4.');
+      }
+      if (!thumbnailFile.mimetype.match(/image\/(jpeg|png)/)) {
+        throw new BadRequestException('Image file must be JPEG or PNG.');
+      }
+
+      const thumbnailFileName = user.uid + '_line_thmb_' + Date.now();
+      const videoFileName = user.uid + '_line_' + Date.now();
+      const [resVideoFile, resThumbnailFile] = await Promise.all([
+        this.uploadService.saveFileOnS3(videoFile, 'line', videoFileName),
+        this.uploadService.saveFileOnS3(
+          thumbnailFile,
+          'line-thumbnail',
+          thumbnailFileName,
+        ),
+      ]);
+
+      if (resVideoFile.status && resThumbnailFile.status) {
+        const video_url = resVideoFile.file_url;
+        const inPath = video_url.split('/').slice(-2).join('/');
+        data.file = video_url;
+        data.thumbnail = resThumbnailFile.file_url;
+        return await this.lineService.createLine(user.uid, data, inPath);
+      } else {
+        throw new HttpException(
+          { error: { code: 'FORBIDDEN' } },
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    } catch (error) {
+      throw new HttpException(
+        { error: { code: 'FORBIDDEN' } },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
+
+  @Post('test')
+  @ApiConsumes('multipart/form-data')
+  // @UseGuards(FirebaseAuthGuard)
+  @Public()
+  @UseInterceptors(
+    AnyFilesInterceptor({ limits: { fileSize: 100 * 1024 * 1024 } }),
+  )
+  async createLines(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Tester() user: any,
     @Body() data: LineCreateDto,
   ) {
     try {
--- src/user/user.decorator.ts
+++ src/user/user.decorator.ts
@@ -9,12 +9,12 @@ export const User = createParamDecorator(
 
 export const Tester = createParamDecorator(() => {
   // return { uid: 'o9WdAztNRVfL2sNtGImD9bARv592' };
+  // return {
+  //   uid: 'o9WdAztNRVfL2sNtGImD9bARv592',
+  //   phone_number: '+8615604034323',
+  // };
   return {
-    uid: 'o9WdAztNRVfL2sNtGImD9bARv592',
-    phone_number: '+8615604034323',
-  };
-  return {
-    uid: '2zwWN6vfcEQ12elcPgAa62nh5WV2',
-    phone_number: '+16555551234',
+    uid: 'keCkanrY76ZNbHPo5vCi19e6BIB3',
+    phone_number: '+34678136148',
   };
 });
